eb410264edbb9c86810ffcfa85b009e76e047f62
duochiqingcai <1193686994@qq.com>
2019-03-19 14:25:31 +0800

    添加抽象类和接口~

---
+++ src/main/java/com/dcqc/demo/基础/接口和抽象类/DemoInterfaceImpl.java
@@ -0,0 +1,14 @@
+package com.dcqc.demo.基础.接口和抽象类;
+
+/**
+ * @program: java-learning
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-03-19 10:52
+ **/
+public class DemoInterfaceImpl implements DemoInterface{
+    @Override
+    public void doOtherthing() {
+        System.out.println("这是接口的方法实现。");
+    }
+}
