0151ad7d34d3c149132495f6a2554560a94cbf02
duochiqingcai <1193686994@qq.com>
2019-03-20 23:58:29 +0800

    添加链表~

---
+++ src/main/java/com/dcqc/demo/数据结构和算法/链表/DemoLinkedList.java
@@ -0,0 +1,13 @@
+package com.dcqc.demo.数据结构和算法.链表;
+
+import java.util.LinkedList;
+
+/**
+ * @program: java-learning
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-03-20 13:57
+ **/
+public class DemoLinkedList {
+
+}
---
+++ src/main/java/com/dcqc/demo/数据结构和算法/链表/单链表逆序.java
@@ -0,0 +1,19 @@
+package com.dcqc.demo.数据结构和算法.链表;
+
+/**
+ * @program: java-learning
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-03-20 23:05
+ **/
+public class 单链表逆序 {
+    //声明head结点
+    private static Node head;
+
+    public static void main(String[] args) {
+
+    }
+
+    //单链表逆序的方法
+
+}
