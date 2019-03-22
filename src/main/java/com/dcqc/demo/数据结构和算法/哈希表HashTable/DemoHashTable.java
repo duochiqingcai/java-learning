6a6037fd1f472ccf8bbfcb53b0d68561490557b5
duochiqingcai <1193686994@qq.com>
2019-03-22 19:17:38 +0800

    补充hashtable~

---
+++ src/main/java/com/dcqc/demo/数据结构和算法/哈希表HashTable/DemoHashTable.java
@@ -0,0 +1,19 @@
+package com.dcqc.demo.数据结构和算法.哈希表HashTable;
+
+import java.util.Hashtable;
+
+/**
+ * @program: java-learning
+ * @description
+ * @author: duochiqingcai
+ * @create: 2019-03-22 13:38
+ **/
+public class DemoHashTable {
+    public static void main(String[] args) {
+        Hashtable<String, String> hashtable = new Hashtable<>();
+        hashtable.put("1", "yihao");
+        hashtable.put("2", "erhao");
+
+        System.out.println(hashtable.hashCode() + hashtable.get("1") + " " + hashtable.get("2"));
+    }
+}
